b4d40546aa50aecaefbaa627f5abe7901cf1b4c2
kostas.kougios <kostas.kougios@googlemail.com>
2011-12-23 22:22:30 +0000

    impl reloading of modified scala classes

--- scalascriptengine/src/main/scala/com/googlecode/scalascriptengine/ScalaClassLoader.scala
+++ scalascriptengine/src/main/scala/com/googlecode/scalascriptengine/ScalaClassLoader.scala
@@ -1,12 +1,16 @@
 package com.googlecode.scalascriptengine
 import java.io.File
-
+import java.util.concurrent.atomic.AtomicInteger
+import scala.tools.nsc.symtab.classfile.ClassfileParser
 /**
  * @author kostantinos.kougios
  *
  * 22 Dec 2011
  */
 class ScalaClassLoader(sourceDirs: Set[File], parentClassLoader: ClassLoader) extends ClassLoader {
+
+	private val cnt = new AtomicInteger
+
 	def this(urls: Set[File]) = this(urls, Thread.currentThread.getContextClassLoader)
 
 	override protected def loadClass(name: String, resolve: Boolean): Class[_] =
@@ -18,13 +22,18 @@ class ScalaClassLoader(sourceDirs: Set[File], parentClassLoader: ClassLoader) ex
 		}
 
 	override protected def findClass(name: String): Class[_] = {
-		val fname = name.replace(".", "/") + ".scala"
-		val srcF = sourceDirs.find(dir => new File(dir, fname).exists).getOrElse(throw new ClassNotFoundException(name))
-		Utils.toBytes(srcF)
+		val fname = name.replace(".", "/") + ".class"
+		val srcF = sourceDirs.map(dir => new File(dir, fname)).find(_.exists).getOrElse(throw new ClassNotFoundException(name))
+		val bytes = Utils.toBytes(srcF)
+		new ThrowawayClassLoader(bytes).get(name)
 	}
 
 	def newInstance[T](className: String): T = {
 		val clz = loadClass(className)
 		clz.newInstance.asInstanceOf[T]
 	}
+}
+
+private class ThrowawayClassLoader(bytes: Array[Byte]) extends ClassLoader {
+	def get(name: String) = defineClass(name, bytes, 0, bytes.length)
 }
