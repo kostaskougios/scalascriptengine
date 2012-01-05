890c398990623a6968fe66cd3ae6e2a228ed8840
kostas.kougios <kostas.kougios@googlemail.com>
2012-01-05 23:11:28 +0000

    constructor functions

--- scalascriptengine/src/main/scala/com/googlecode/scalascriptengine/Constructors.scala
+++ scalascriptengine/src/main/scala/com/googlecode/scalascriptengine/Constructors.scala
@@ -8,21 +8,63 @@ package com.googlecode.scalascriptengine
  * 5 Jan 2012
  */
 class Constructors[T](clz: Class[T]) {
+
 	def newInstance = clz.newInstance
+
 	def newInstance[P1](p1: P1)(implicit p1m: ClassManifest[P1]): T = {
 		val c = clz.getConstructor(p1m.erasure)
 		c.newInstance(Array(p1.asInstanceOf[Object]): _*)
 	}
+
 	def newInstance[P1, P2](p1: P1, p2: P2)(implicit p1m: ClassManifest[P1], p2m: ClassManifest[P2]): T = {
 		val c = clz.getConstructor(p1m.erasure, p2m.erasure)
 		c.newInstance(Array(p1.asInstanceOf[Object], p2.asInstanceOf[Object]): _*)
 	}
+
 	def newInstance[P1, P2, P3](p1: P1, p2: P2, p3: P3)(implicit p1m: ClassManifest[P1], p2m: ClassManifest[P2], p3m: ClassManifest[P3]): T = {
 		val c = clz.getConstructor(p1m.erasure, p2m.erasure, p3m.erasure)
 		c.newInstance(Array(p1.asInstanceOf[Object], p2.asInstanceOf[Object], p3.asInstanceOf[Object]): _*)
 	}
+
 	def newInstance[P1, P2, P3, P4](p1: P1, p2: P2, p3: P3, p4: P4)(implicit p1m: ClassManifest[P1], p2m: ClassManifest[P2], p3m: ClassManifest[P3], p4m: ClassManifest[P4]): T = {
 		val c = clz.getConstructor(p1m.erasure, p2m.erasure, p3m.erasure, p4m.erasure)
 		c.newInstance(Array(p1.asInstanceOf[Object], p2.asInstanceOf[Object], p3.asInstanceOf[Object], p4.asInstanceOf[Object]): _*)
+	}
+
+	/**
+	 * returns a function that can be used to instantiate the class using a no-arg constructor
+	 */
+	def constructor = newInstance _
+
+	/**
+	 * returns a function that can be used to instantiate the class using 1 arg constructor
+	 */
+	def constructorWith1Arg[P1](implicit p1m: ClassManifest[P1]): P1 => T = {
+		val c = clz.getConstructor(p1m.erasure)
+		(p1: P1) => c.newInstance(Array(p1.asInstanceOf[Object]): _*)
+	}
+
+	/**
+	 * returns a function that can be used to instantiate the class using 2 arg constructor
+	 */
+	def constructorWith2Args[P1, P2](implicit p1m: ClassManifest[P1], p2m: ClassManifest[P2]): (P1, P2) => T = {
+		val c = clz.getConstructor(p1m.erasure, p2m.erasure)
+		(p1: P1, p2: P2) => c.newInstance(Array(p1.asInstanceOf[Object], p2.asInstanceOf[Object]): _*)
+	}
+
+	/**
+	 * returns a function that can be used to instantiate the class using 3 arg constructor
+	 */
+	def constructorWith3Args[P1, P2, P3](implicit p1m: ClassManifest[P1], p2m: ClassManifest[P2], p3m: ClassManifest[P3]): (P1, P2, P3) => T = {
+		val c = clz.getConstructor(p1m.erasure, p2m.erasure, p3m.erasure)
+		(p1: P1, p2: P2, p3: P3) => c.newInstance(Array(p1.asInstanceOf[Object], p2.asInstanceOf[Object], p3.asInstanceOf[Object]): _*)
+	}
+
+	/**
+	 * returns a function that can be used to instantiate the class using 3 arg constructor
+	 */
+	def constructorWith4Args[P1, P2, P3, P4](implicit p1m: ClassManifest[P1], p2m: ClassManifest[P2], p3m: ClassManifest[P3], p4m: ClassManifest[P4]): (P1, P2, P3, P4) => T = {
+		val c = clz.getConstructor(p1m.erasure, p2m.erasure, p3m.erasure, p4m.erasure)
+		(p1: P1, p2: P2, p3: P3, p4: P4) => c.newInstance(Array(p1.asInstanceOf[Object], p2.asInstanceOf[Object], p3.asInstanceOf[Object], p4.asInstanceOf[Object]): _*)
 	}
 }
--- scalascriptengine/src/test/scala/com/googlecode/scalascriptengine/ConstructorsSuite.scala
+++ scalascriptengine/src/test/scala/com/googlecode/scalascriptengine/ConstructorsSuite.scala
@@ -21,15 +21,31 @@ class ConstructorsSuite extends FunSuite with ShouldMatchers {
 		t.s should be === "noarg"
 	}
 
+	test("no arg constructor function") {
+		val t = constructors.constructor
+		t().s should be === "noarg"
+	}
+
 	test("one arg constructor") {
 		val t = constructors.newInstance("one-arg")
 		t.s should be === "one-arg"
+	}
+
+	test("one arg constructor function") {
+		val t = constructors.constructorWith1Arg[String]
+		t("one-arg").s should be === "one-arg"
 	}
 
 	test("two arg constructor") {
 		val t = constructors.newInstance("two-arg", 2)
 		t.s should be === "two-arg"
 		t.x should be === 2
+	}
+
+	test("two arg constructor function") {
+		val t = constructors.constructorWith2Args[String, Int]
+		t("two-arg", 2).s should be === "two-arg"
+		t("two-arg", 2).x should be === 2
 	}
 
 	test("three arg constructor") {
@@ -39,12 +55,29 @@ class ConstructorsSuite extends FunSuite with ShouldMatchers {
 		t.y should be === 5.toDouble
 	}
 
+	test("three arg constructor function") {
+		val t = constructors.constructorWith3Args[String, Int, Double]
+		val v = t("three-arg", 3, 5.toDouble)
+		v.s should be === "three-arg"
+		v.x should be === 3
+		v.y should be === 5.toDouble
+	}
+
 	test("four arg constructor") {
 		val t = constructors.newInstance("four-arg", 3, 5.toDouble, true)
 		t.s should be === "four-arg"
 		t.x should be === 3
 		t.y should be === 5.toDouble
 		t.z should be(true)
+	}
+
+	test("four arg constructor function") {
+		val t = constructors.constructorWith4Args[String, Int, Double, Boolean]
+		val v = t("four-arg", 3, 5.toDouble, true)
+		v.s should be === "four-arg"
+		v.x should be === 3
+		v.y should be === 5.toDouble
+		v.z should be(true)
 	}
 }
 
