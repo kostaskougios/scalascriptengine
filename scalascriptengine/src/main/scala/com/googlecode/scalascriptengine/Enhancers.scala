9559f07f2ec4689f2e8b3d62fa26641e577c5b34
kostas.kougios <kostas.kougios@googlemail.com>
2012-04-03 13:52:28 +0000

    if version available then use version for ClassPath enhancer

--- scalascriptengine/src/main/scala/com/googlecode/scalascriptengine/Enhancers.scala
+++ scalascriptengine/src/main/scala/com/googlecode/scalascriptengine/Enhancers.scala
@@ -1,9 +1,12 @@
 package com.googlecode.scalascriptengine
 
 /**
+ * if no code version is ready,
  * tries to find the classes from the classpath initially. If that
  * fails, it then goes the route of finding/compiling a scala class
  * from the source classpaths.
+ *
+ * If a codeversion is ready, it just uses the codeversion
  *
  * This can come handy i.e. during development if your IDE compiles
  * the dynamic scala classes and you wouldn't like to wait for the
@@ -15,9 +18,11 @@ package com.googlecode.scalascriptengine
  */
 trait FromClasspathFirst extends ScalaScriptEngine {
 	abstract override def get[T](className: String): Class[T] =
-		try {
-			Class.forName(className).asInstanceOf[Class[T]]
-		} catch {
-			case _: ClassNotFoundException => super.get(className)
-		}
+		if (currentVersion.version == 0) {
+			try {
+				Class.forName(className).asInstanceOf[Class[T]]
+			} catch {
+				case _: ClassNotFoundException => super.get(className)
+			}
+		} else super.get(className)
 }
