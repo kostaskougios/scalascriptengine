c8d02de606cb5bcc7ab26f1b86f6cd4d2549b0cf
kostas.kougios <kostas.kougios@googlemail.com>
2012-08-25 00:44:41 +0100

    comments for eval

--- scalascriptengine/src/test/scala/com/googlecode/scalascriptengine/EvalCodeSuite.scala
+++ scalascriptengine/src/test/scala/com/googlecode/scalascriptengine/EvalCodeSuite.scala
@@ -14,15 +14,28 @@ import org.scalatest.matchers.ShouldMatchers
 class EvalCodeSuite extends FunSuite with ShouldMatchers {
 
 	test("using functions") {
+		// create a factory that evaluates code that takes 1 string param and returns an Int.
+		// The string param is named s and the evaluated code is s.toInt.
+		// In other words, this creates a function:
+		// (s:String)=>s.toInt
 		val ect = EvalCode[String => Int]("s" :: Nil, "s.toInt")
 
+		// Now create a new instance of this function
 		val x = ect.newInstance
+
+		// evaluates f("17") = "17".toInt
 		x("17") should be === 17
 	}
 
 	test("constructs src code correctly, 2 args") {
+		// creates a factory for instantiating a function (Float, Double) => Double
+		// as (i1:Float,i2:Double)=>i1+i2
 		val ect = EvalCode[(Float, Double) => Double]("i1" :: "i2" :: Nil, "i1 + i2")
+
+		// create a new instance of the function
 		val x = ect.newInstance
+
+		// and apply it
 		x(12.5f, 2.5) should be === 15.0
 	}
 
