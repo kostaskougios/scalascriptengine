37cb623bb9d7cd4bbbacc18c7f7fde04fc35f968
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2014-05-15 11:48:18 +0100

    simplifying compilation plugin

--- scalascriptengine/src/main/scala/com/googlecode/scalascriptengine/CompilationPlugins.scala
+++ scalascriptengine/src/main/scala/com/googlecode/scalascriptengine/CompilationPlugins.scala
@@ -1,6 +1,5 @@
 package com.googlecode.scalascriptengine
 
-import scala.tools.nsc.plugins.PluginComponent
 import scala.tools.nsc.Global
 import scala.tools.nsc.Phase
 
@@ -12,29 +11,20 @@ import scala.tools.nsc.Phase
  *
  *         8 Jan 2012
  */
-class CompilationPlugins(val global: Global, sse: ScalaScriptEngine) extends PluginComponent
+class CompilationPlugins(val global: Global, sse: ScalaScriptEngine)
 {
-
-	val runsAfter = List("typer")
-	val phaseName = CompilationPlugins.PhaseName
-
 	def newPhase(prev: Phase) = new PrePhase(prev, prev.next)
 
-	class PrePhase(prev: Phase, nxt: Phase) extends StdPhase(prev) with Logging
+	class PrePhase(prev: Phase, nxt: Phase) extends global.GlobalPhase(prev) with Logging
 	{
-		override def name = CompilationPlugins.PhaseName
+		override def name = "sse-compilation-plugins"
 
 		override def next = nxt
 
-		def apply(unit: global.CompilationUnit) {
+		override def apply(unit: global.CompilationUnit) {
 			info("compiling unit " + unit)
 			sse.compilationStatus.checkStop
 		}
 	}
 
-}
-
-object CompilationPlugins
-{
-	val PhaseName = "sse-compilation-plugins"
 }
